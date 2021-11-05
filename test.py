486dfc13b183a57b2eeca352f848f22e70a54b80
armando bibay jr <apple@apples-MacBook-Pro.local>
2021-11-06 02:41:00 +0800

    Initial Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# Printing Hello World
+
+msg = "Hello Worlds"
+print(msg.upper())
+
+# Declaring Variable
+
+age = 23
+full_name = "Armando I. Bibay Jr"
+price = 29.40
+
+print(f"I am {full_name} and I am {age} years old")
+
+# Receiving input from user
+
+name = input("What is your name? ")
+print("The name is " + name)
+
+# Type Conversion
