0499f2b01f1a0a5c181ad5e9cfd84353b0169b6c
armando bibay jr <apple@apples-MacBook-Pro.local>
2021-11-06 03:03:01 +0800

    String Manipulations

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # Printing Hello World
 
-msg = "Hello Worlds"
+msg = "Hello, World"
 print(msg.upper())
 
 # Declaring Variable
@@ -16,4 +16,30 @@ print(f"I am {full_name} and I am {age} years old")
 name = input("What is your name? ")
 print("The name is " + name)
 
-# Type Conversion
+# Type Conversion i.e. int(), float(), bool(), str()
+
+birth_year = input("What is your birth year?" )
+age = 2021 - int(birth_year)
+print(f"The age is {age}")
+
+# Adding Two Numbers
+
+first_number =  float(input("Enter first Number: "))
+second_number = float(input("Enter Second Number: "))
+sum= first_number + second_number
+print(f"The sum of two numbers is {sum}")
+
+
+# String Manipulation
+
+course = "Python Course For Beginners"
+course_in_lowercase = course.lower()
+course_in_uppercase = course.upper()
+course_replaced_with_lesson = course.replace('Course', "Lesson")
+is_the_word_Python_existing_in_the_course = "Python" in course #returns a boolean
+
+print(f"The course is called: {course}")
+print(f"The course in lowercase: {course_in_lowercase}")
+print(f"The course in uppercase: {course_in_uppercase}")
+print(f"The word course replaced with {course_replaced_with_lesson}")
+print(f"The course has the word Python in it: {is_the_word_Python_existing_in_the_course}")
